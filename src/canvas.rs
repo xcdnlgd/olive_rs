b27031118075bd1666830a2a51834a104ceaf3ec
xcdnlgd <xcdnlgd@hotmail.com>
2024-09-21 23:45:24 +0800

    Ray gives you next x and y, use this to draw line, may be useful for triangle

--- src/canvas.rs
+++ src/canvas.rs
@@ -7,24 +7,146 @@ use std::{
 #[allow(non_camel_case_types)]
 pub type u31 = u32;
 
+pub struct Canvas {
+    pub width: u31,
+    pub height: u31,
+    buffer: Vec<u32>,
+}
+impl Canvas {
+    pub fn new(width: u31, height: u31) -> Self {
+        Self {
+            width,
+            height,
+            buffer: vec![0u32; (width * height) as usize],
+        }
+    }
+    pub fn draw_line(&mut self, x0: i32, y0: i32, x1: i32, y1: i32, color: u32) {
+        let (x0, y0, x1, y1) = if let Some(Line2D { x0, y0, x1, y1 }) = (Line2D {
+            x0: x0 as f32,
+            y0: y0 as f32,
+            x1: x1 as f32,
+            y1: y1 as f32,
+        })
+        .box_clip(
+            0f32,
+            0f32,
+            self.width as f32 - 0.1,
+            self.height as f32 - 0.1,
+        ) {
+            (x0 as i32, y0 as i32, x1 as i32, y1 as i32)
+        } else {
+            return;
+        };
+        self.draw_pixel_unchecked(x1 as u31, y1 as u31, color);
+
+        let mut ray = Ray::new(x0, y0, x1, y1);
+        while !ray.reached {
+            let (x, y) = ray.next_xy();
+            self.draw_pixel_unchecked(x as u31, y as u31, color)
+        }
+    }
+    pub fn fill(&mut self, color: u32) {
+        self.buffer.iter_mut().for_each(|pixel| *pixel = color);
+    }
+    pub fn fill_circle(&mut self, center_x: i32, center_y: i32, r: u31, color: u32) {
+        let r = r as i32;
+
+        if center_x + r < 0
+            || center_y + r < 0
+            || (center_x - r) as u31 > self.width
+            || (center_y - r) as u31 > self.height
+        {
+            return;
+        }
+
+        // bresenham
+        // Taylor Expansion to get rid of sqrt
+        // and other approximation
+        let mut x0 = 0;
+        let mut y0 = r;
+        let mut d = 3 - 2 * r;
+
+        // only formulate the arc above the line y=x in the first quadrant.
+        // iterate x
+        while y0 >= x0 {
+            for i in center_x - x0..=center_x + x0 {
+                self.draw_pixel(i, center_y + y0, color);
+            }
+            for i in center_x - y0..=center_x + y0 {
+                self.draw_pixel(i, center_y + x0, color);
+            }
+            for i in center_x - y0..=center_x + y0 {
+                self.draw_pixel(i, center_y - x0, color);
+            }
+            for i in center_x - x0..=center_x + x0 {
+                self.draw_pixel(i, center_y - y0, color);
+            }
+            if d < 0 {
+                d += 4 * x0 + 6;
+            } else {
+                d += 4 * (x0 - y0) + 10;
+                y0 -= 1;
+            }
+            x0 += 1;
+        }
+    }
+    pub fn fill_rect(&mut self, x0: i32, y0: i32, w: u31, h: u31, color: u32) {
+        let xn = (x0 + w as i32).clamp(0, self.width as i32) as u31;
+        let yn = (y0 + h as i32).clamp(0, self.height as i32) as u31;
+        let x0 = x0.max(0) as u31;
+        let y0 = y0.max(0) as u31;
+        for y in y0..yn {
+            for x in x0..xn {
+                self.draw_pixel_unchecked(x as u31, y as u31, color);
+            }
+        }
+    }
+    pub fn save_to_ppm_file(&self, path: impl AsRef<Path>) -> std::io::Result<()> {
+        let file = File::create(path)?;
+        let mut file = BufWriter::new(file);
+        write!(file, "P6\n{} {} 255\n", self.width, self.height)?;
+        for &pixel in self.buffer.iter() {
+            let rgb: [u8; 3] = [
+                ((pixel) & 0xFF) as u8,
+                ((pixel >> 8) & 0xFF) as u8,
+                ((pixel >> (8 * 2)) & 0xFF) as u8,
+            ];
+            file.write_all(&rgb)?;
+        }
+        Ok(())
+    }
+}
+impl Canvas {
+    fn draw_pixel(&mut self, x: i32, y: i32, color: u32) {
+        if x < 0 || y < 0 {
+            return;
+        }
+        let x = x as u31;
+        let y = y as u31;
+        if x < self.width && y < self.height {
+            self.buffer[(y * self.width + x) as usize] = color;
+        }
+    }
+    #[inline]
+    fn draw_pixel_unchecked(&mut self, x: u31, y: u31, color: u32) {
+        self.buffer[(y * self.width + x) as usize] = color;
+    }
+}
+
 #[derive(Clone)]
-struct RectArea {
+struct Line2D {
     x0: f32,
     y0: f32,
     x1: f32,
     y1: f32,
 }
-impl RectArea {
-    fn box_clip(&self, limit: &Self) -> Option<Self> {
-        let mut x_min = limit.x0;
-        let mut x_max = limit.x1;
+impl Line2D {
+    fn box_clip(&self, x_min: f32, y_min: f32, x_max: f32, y_max: f32) -> Option<Self> {
         if x_max < x_min {
-            std::mem::swap(&mut x_min, &mut x_max);
+            return None;
         }
-        let mut y_min = limit.y0;
-        let mut y_max = limit.y1;
         if y_max < y_min {
-            std::mem::swap(&mut y_min, &mut y_max);
+            return None;
         }
 
         // Cohen–Sutherland
@@ -115,52 +237,23 @@ impl RectArea {
     }
 }
 
-pub struct Canvas {
-    pub width: u31,
-    pub height: u31,
-    buffer: Vec<u32>,
+pub struct Ray {
+    pub x0: i32,
+    pub y0: i32,
+    pub x1: i32,
+    pub y1: i32,
+    pub reached: bool,
+    sx: i32,
+    sy: i32,
+    dx: i32,
+    dy: i32,
+    error: i32,
+    x: i32,
+    y: i32,
+    next: fn(&mut Self) -> (i32, i32),
 }
-impl Canvas {
-    pub fn new(width: u31, height: u31) -> Self {
-        Self {
-            width,
-            height,
-            buffer: vec![0u32; (width * height) as usize],
-        }
-    }
-    fn draw_pixel(&mut self, x: i32, y: i32, color: u32) {
-        if x < 0 || y < 0 {
-            return;
-        }
-        let x = x as u31;
-        let y = y as u31;
-        if x < self.width && y < self.height {
-            self.buffer[(y * self.width + x) as usize] = color;
-        }
-    }
-    #[inline]
-    fn draw_pixel_unchecked(&mut self, x: u31, y: u31, color: u32) {
-        self.buffer[(y * self.width + x) as usize] = color;
-    }
-    pub fn draw_line(&mut self, x0: i32, y0: i32, x1: i32, y1: i32, color: u32) {
-        let (x0, y0, x1, y1) = if let Some(RectArea { x0, y0, x1, y1 }) = (RectArea {
-            x0: x0 as f32,
-            y0: y0 as f32,
-            x1: x1 as f32,
-            y1: y1 as f32,
-        })
-        .box_clip(&RectArea {
-            x0: 0f32,
-            y0: 0f32,
-            x1: self.width as f32 - 0.1,
-            y1: self.height as f32 - 0.1,
-        }) {
-            (x0 as i32, y0 as i32, x1 as i32, y1 as i32)
-        } else {
-            return;
-        };
-        self.draw_pixel_unchecked(x1 as u31, y1 as u31, color);
-
+impl Ray {
+    pub fn new(x0: i32, y0: i32, x1: i32, y1: i32) -> Self {
         // bresenham
         let sx = if x1 < x0 { -1 } else { 1 };
         let sy = if y1 < y0 { -1 } else { 1 };
@@ -168,100 +261,61 @@ impl Canvas {
         let dx = (x1 - x0).abs();
         let dy = (y1 - y0).abs();
 
-        let mut x = x0;
-        let mut y = y0;
+        let x = x0;
+        let y = y0;
+
+        let error;
+        let next: fn(&mut Self) -> (i32, i32);
+        let reached;
         if dy < dx {
-            let mut error = -dx;
-            while x != x1 {
-                self.draw_pixel_unchecked(x as u31, y as u31, color);
-                error += dy + dy;
-                if error >= 0 {
-                    y += sy;
-                    error -= dx + dx;
-                }
-                x += sx;
-            }
+            reached = x == x1;
+            error = -dx;
+            next = Self::iter_x;
         } else {
-            let mut error = -dy;
-            while y != y1 {
-                self.draw_pixel_unchecked(x as u31, y as u31, color);
-                error += dx + dx;
-                if error >= 0 {
-                    x += sx;
-                    error -= dy + dy;
-                }
-                y += sy;
-            }
-        }
-    }
-    pub fn fill(&mut self, color: u32) {
-        self.buffer.iter_mut().for_each(|pixel| *pixel = color);
-    }
-    pub fn fill_circle(&mut self, center_x: i32, center_y: i32, r: u31, color: u32) {
-        let r = r as i32;
-
-        if center_x + r < 0
-            || center_y + r < 0
-            || (center_x - r) as u31 > self.width
-            || (center_y - r) as u31 > self.height
-        {
-            return;
+            reached = y == y1;
+            error = -dy;
+            next = Self::iter_y;
         }
 
-        // bresenham
-        // Taylor Expansion to get rid of sqrt
-        // and other approximation
-        let mut x0 = 0;
-        let mut y0 = r;
-        let mut d = 3 - 2 * r;
-
-        // only formulate the arc above the line y=x in the first quadrant.
-        // iterate x
-        while y0 >= x0 {
-            for i in center_x - x0..=center_x + x0 {
-                self.draw_pixel(i, center_y + y0, color);
-            }
-            for i in center_x - y0..=center_x + y0 {
-                self.draw_pixel(i, center_y + x0, color);
-            }
-            for i in center_x - y0..=center_x + y0 {
-                self.draw_pixel(i, center_y - x0, color);
-            }
-            for i in center_x - x0..=center_x + x0 {
-                self.draw_pixel(i, center_y - y0, color);
-            }
-            if d < 0 {
-                d += 4 * x0 + 6;
-            } else {
-                d += 4 * (x0 - y0) + 10;
-                y0 -= 1;
-            }
-            x0 += 1;
+        Self {
+            x0,
+            y0,
+            x1,
+            y1,
+            reached,
+            sx,
+            sy,
+            dx,
+            dy,
+            error,
+            x,
+            y,
+            next,
         }
     }
-    pub fn fill_rect(&mut self, x0: i32, y0: i32, w: u31, h: u31, color: u32) {
-        let xn = (x0 + w as i32).clamp(0, self.width as i32) as u31;
-        let yn = (y0 + h as i32).clamp(0, self.height as i32) as u31;
-        let x0 = x0.max(0) as u31;
-        let y0 = y0.max(0) as u31;
-        for y in y0..yn {
-            for x in x0..xn {
-                self.draw_pixel_unchecked(x as u31, y as u31, color);
-            }
-        }
+    pub fn next_xy(&mut self) -> (i32, i32) {
+        (self.next)(self)
     }
-    pub fn save_to_ppm_file(&self, path: impl AsRef<Path>) -> std::io::Result<()> {
-        let file = File::create(path)?;
-        let mut file = BufWriter::new(file);
-        write!(file, "P6\n{} {} 255\n", self.width, self.height)?;
-        for &pixel in self.buffer.iter() {
-            let rgb: [u8; 3] = [
-                ((pixel) & 0xFF) as u8,
-                ((pixel >> 8) & 0xFF) as u8,
-                ((pixel >> (8 * 2)) & 0xFF) as u8,
-            ];
-            file.write_all(&rgb)?;
+    fn iter_x(&mut self) -> (i32, i32) {
+        let result = (self.x, self.y);
+        self.error += self.dy + self.dy;
+        if self.error >= 0 {
+            self.y += self.sy;
+            self.error -= self.dx + self.dx;
         }
-        Ok(())
+        self.x += self.sx;
+        self.reached = self.x == self.x1;
+        result
+    }
+    fn iter_y(&mut self) -> (i32, i32) {
+        let result = (self.x, self.y);
+        self.error += self.dx + self.dx;
+        if self.error >= 0 {
+            self.x += self.sx;
+            self.error -= self.dy + self.dy;
+        }
+        self.y += self.sy;
+        self.reached = self.y == self.y1;
+        result
     }
 }
